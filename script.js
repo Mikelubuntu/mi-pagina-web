a4f17fe2842be82c8c7ba696c8496f33a1476b74
Mikelubuntu <mikel.hernandez@casaricardo.com>
2025-02-27 08:36:51 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function mostrarSeccion(seccionId) {
+    // Ocultar todas las secciones
+    const secciones = document.querySelectorAll('.seccion');
+    secciones.forEach(seccion => {
+      seccion.style.display = 'none';
+    });
+  
+    // Mostrar la sección seleccionada
+    const seccionMostrar = document.getElementById(seccionId);
+    if (seccionMostrar) {
+      seccionMostrar.style.display = 'block';
+    }
+  }
+  
